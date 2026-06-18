98987ba51f519f8506ba403ba87b85d0b978206e
Bastian Santander <40833839+bsantander@users.noreply.github.com>
2026-06-18 15:36:53 -0400

    vista mantenciones

---
+++ models/Mod_Mantenciones.php
@@ -0,0 +1,14 @@
+<?php
+class Mod_Mantenciones {
+    private $conexion;
+
+    public function __construct($conexion) {
+        $this->conexion = $conexion;
+    }
+
+    public function crear_mant_correctiva(){
+        
+    }
+
+}
+?>
