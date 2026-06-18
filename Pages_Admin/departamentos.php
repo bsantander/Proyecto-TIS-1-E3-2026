aed426df9e9630d760ef6667a2b4b83d8b7f4d21
SaturnYongo <hachibogard@gmail.com>
2026-06-17 23:22:48 -0400

    corrigiendo un problema

--- Pages_Admin/departamentos.php
+++ Pages_Admin/departamentos.php
@@ -318,7 +318,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 </script>
->>>>>>> origin/molavef
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
