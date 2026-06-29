dbf94f368ba7facceccb9d51f20fde2201768c44
Alopezm22 <alopezm@ing.ucsc.cl>
2026-06-28 20:54:21 -0400

    Mejoras equipos

--- models/Mod_Equipos.php
+++ models/Mod_Equipos.php
@@ -10,15 +10,18 @@ function _obtenerTabla($tipo) {
     ];
     return $mapa[$tipo] ?? null;
 }
-
 function obtenerDatosCompletos($conexion, $id, $tipo) {
     $tabla = _obtenerTabla($tipo);
+    
     $sql = "SELECT $tabla.*, 
-                   funcionario.nombre_completo AS nombre_funcionario, 
+                   funcionario.nombre_completo AS nombre_funcionario,
+                   funcionario.id_funcionario,
+                   departamento.nombre_departamento AS nombre_departamento,
                    proveedor.nombre_completo AS nombre_proveedor, 
                    proveedor.contacto AS contacto_proveedor
             FROM $tabla
-            LEFT JOIN funcionario ON $tabla.id_funcionario = funcionario.id_funcionario 
+            LEFT JOIN funcionario ON $tabla.id_funcionario = funcionario.id_funcionario
+            LEFT JOIN departamento ON funcionario.id_departamento = departamento.id_departamento
             LEFT JOIN proveedor ON $tabla.id_proveedor = proveedor.id_proveedor
             WHERE $tabla.id_equipo = $id";
             
@@ -26,25 +29,28 @@ function obtenerDatosCompletos($conexion, $id, $tipo) {
     $row = mysqli_fetch_assoc($resultado);
     
     $funcionario = [
-        'Nombre Completo' => $row['nombre_funcionario'] ?? 'Sin asignar'
+        'id_funcionario'      => $row['id_funcionario'],
+        'nombre'              => $row['nombre_funcionario'],
+        'nombre_departamento' => $row['nombre_departamento']
     ];
     
     $proveedor = [
-        'id_proveedor'    => $row['id_proveedor'] ?? 0,
-        'nombre_completo' => $row['nombre_proveedor'] ?? 'Sin asignar',
-        'contacto'        => $row['contacto_proveedor'] ?? 'Sin contacto'
+        'id_proveedor'    => $row['id_proveedor'],
+        'nombre_completo' => $row['nombre_proveedor'],
+        'contacto'        => $row['contacto_proveedor']
     ];
     
-    unset($row['nombre_funcionario'], $row['nombre_proveedor'], $row['contacto_proveedor']);
+    unset($row['nombre_funcionario'], $row['nombre_proveedor'], $row['contacto_proveedor'], $row['nombre_departamento']);
     
     return [
-        'equipo' => $row,
+        'equipo'      => $row,
         'funcionario' => $funcionario,
-        'proveedor' => $proveedor
+        'proveedor'   => $proveedor
     ];
 }
 function obtenerTodosFuncionarios($conexion) {
-    $sql = "SELECT id_funcionario, nombre_completo FROM funcionario";
+    $sql = "SELECT funcionario.id_funcionario, funcionario.nombre_completo, departamento.nombre_departamento FROM funcionario 
+            LEFT JOIN departamento on funcionario.id_departamento = departamento.id_departamento";
     $resultado = mysqli_query($conexion, $sql);
     $funcionarios = [];
     while ($row = mysqli_fetch_assoc($resultado)) {
