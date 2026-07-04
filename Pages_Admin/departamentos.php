52f4258fd8728f03dc64f87b74c0229653af610c
Matito <86012198+MatiasOlave1@users.noreply.github.com>
2026-07-04 13:25:19 -0400

    background sidebar arreglado

--- Pages_Admin/departamentos.php
+++ Pages_Admin/departamentos.php
@@ -77,7 +77,7 @@
 
     <div class="Container d-flex flex-row vh-100 overflow-hidden">
 
-        <div class="Barra_Lateral d-flex flex-column justify-content-between p-3">
+        <div class="Barra_Lateral d-flex flex-column justify-content-between p-3" style="background-color: #BBBFBF;">
             <div class="Superior d-flex flex-column justify-content-start align-items-start gap-2">
 
                 <div class="Inicio p-2 d-flex flex-row justify-content-start gap-0 ">
