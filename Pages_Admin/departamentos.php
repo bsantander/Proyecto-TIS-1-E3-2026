7bc9a5962150d96327d042483850eb0a8f203eaf
Matito <86012198+MatiasOlave1@users.noreply.github.com>
2026-07-04 14:04:41 -0400

    ajuste

--- Pages_Admin/departamentos.php
+++ Pages_Admin/departamentos.php
@@ -131,14 +131,14 @@
                 <div class="Configuracion" >
                     <a href="#" class=" d-flex flex-row justify-content-start gap-2 align-items-center text-decoration-none text-black p-2">
                         <span class="material-symbols-outlined">build</span>                   
-                        <p class="m-0 fs-6">Configuración</p>
+                        <p class="m-0 fs-6">Configuracion</p>
                     </a>
                 </div>
 
                 <div class="Cerrar_Sesion">
                     <a href="../secion.php?logout=1" class=" d-flex flex-row justify-content-start gap-2 align-items-center text-decoration-none text-danger p-2">
                         <span class="material-symbols-outlined">logout</span>
-                        <p class="m-0 fs-6">Cerrar Sesión</p>
+                        <p class="m-0 fs-6">Cerrar Sesion</p>
                     </a>
                 </div>        
             </div>
