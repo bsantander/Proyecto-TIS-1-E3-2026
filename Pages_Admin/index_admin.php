65d66e2bd310b952859906459c061e109b953616
Matito <86012198+MatiasOlave1@users.noreply.github.com>
2026-07-08 23:22:18 -0400

    editando sidebar

--- Pages_Admin/index_admin.php
+++ Pages_Admin/index_admin.php
@@ -52,7 +52,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../assets/style.css?v=8">
     <script src="../assets/script.js" defer></script>
     <script src="../assets/dashboard.js" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
